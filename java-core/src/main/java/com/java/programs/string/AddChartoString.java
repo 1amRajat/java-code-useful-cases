5510f0dd2845d6f56bbc628aa19823efa541ae90
User <rajat.karmakar06@gmail.com>
2025-11-01 20:50:13 +0530

    draft project

---
+++ java-core/src/main/java/com/java/programs/string/AddChartoString.java
@@ -0,0 +1,14 @@
+package com.java.programs.string;
+
+public class AddChartoString {
+
+	public static void main(String[] args) {
+		// Add a character to a string
+		String input = "ParentString";
+		char toAdd = 's';
+		String output = input + toAdd;
+		System.out.println(output);
+
+	}
+
+}
